c2c4d85cbffbc2f26e7e7edd6d18f314ce71873e
coderawii <coderawii@gmail.com.com>
2021-02-18 18:25:17 +0100

    initial commit
    prvi deo do api key-a

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="container">
+    <AppHeader />
+    <AppRestaurantInfo />
+    <!-- <AppRestaurantInfo :datasource="fooddata" /> -->
+  </main>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppRestaurantInfo
+    // AppRestaurantInfo: () =>
+    //   import(/* webpackPrefetch: true */ "@/components/AppRestaurantInfo.vue")
+  }
+  // computed: {
+  //   fooddata() {
+  //     return this.$store.state.fooddata;
+  //   },
+  // },
+};
+</script>
+
+<style></style>
---
+++ plugins/getfood.server.js
@@ -0,0 +1,4 @@
+export default async ({ store }) => {
+  // ! kao mogli bismo ovo da radimo u index page na klajentu, ali zasto da teretimo klajent sajd, kad lkepo mozemo ovo da odradimo na serveru. zato kreiramo u plugin js fajl sa SERVER imenom u sebi. i sad moramo u nuxt.config.js da mu damo do znanja da taj fajl/plugin postoji
+  await store.dispatch("getFoodData");
+};
